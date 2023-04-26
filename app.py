95b9bc8480d6405c9851acc698dd9e769d8313b3
nisargee <nisargeeraval0402@gmail.com>
2023-04-26 15:49:30 +0530

    testing

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CLEAR = 'clear'
 
 # Define the app
 app = Flask(__name__)
-
+app.secret_key = "nisargee"
 # Configure the folder paths for the app
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['BLURRED'] = BLURRED
@@ -66,67 +66,70 @@ def index():
 @app.route('/submit', methods = ["POST", "GET"])
 def submit():
     # if the request method is post, i.e, the input is submitted
-    if request.method == "POST":
-        # if required input is not found
-        if 'ctrl' not in request.files:
-            # Show a flash message and redirect to the same page
-            flash('No file part')
-            return redirect(request.url)
-        else:
-            data = request.files.getlist('ctrl')
-            
-            # If user has entered a valid threshold, use it else set threshold to default value 81
-            try:
-                threshold = int(request.form['threshold'])
-            except:
-                threshold = 81
-            
-            # cLear all the pre-existing files: to minimise the amount of data stored at the server
-            clear_folders()  
-
-            # iterate through the input files
-            for file in data:
-                
-                # check if valid file, save it
-                if file and allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-                # if file invalid, return error message
-                else:
-                    return render_template('index.html', download = False, message='Invalid file format!')
-            
-            # iterate through the input images
-            for file in data:
-                filename = secure_filename(file.filename)
-
-                # read file as image using cv2
-                image = cv2.imread(app.config['UPLOAD_FOLDER'] +'\\'+ filename)
-
-                # convert into grayscale image
-                imagebw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-                # finds focus measure of the image
-                ''' Focus measure is the degree of sharpness of an image i.e., so higher focus measure, more clear image.
-                Focus measure is calculated by finding gradient of gaussian filter or a laplacian filter on an image. 
-                Here, we use Laplacian filter. '''
-                focus_measure = cv2.Laplacian(imagebw, cv2.CV_64F).var() 
-
-                # If focus measure < threshold, the image is blur, so save in blurred folder
-                if focus_measure < threshold: 
-                    cv2.imwrite('./blurred/' + filename, image) 
+    try:
+        if request.method == "POST":
+            # if required input is not found
+            if 'ctrl' not in request.files:
+                # Show a flash message and redirect to the same page
+                flash('No file part')
+                return redirect(request.url)
+            else:
+                data = request.files.getlist('ctrl')
+                flash('got the files')
+                # If user has entered a valid threshold, use it else set threshold to default value 81
+                try:
+                    threshold = int(request.form['threshold'])
+                except:
+                    threshold = 81
+                flash('set the threshold')
+                # cLear all the pre-existing files: to minimise the amount of data stored at the server
+                clear_folders()  
+                flash('cleared folders')
+                # iterate through the input files
+                for file in data:
                     
-                # If focus measure >= threshold, the image is clear, so save in clear folder
-                else:
-                    cv2.imwrite('./clear/' + filename, image) 
+                    # check if valid file, save it
+                    if file and allowed_file(file.filename):
+                        filename = secure_filename(file.filename)
+                        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        # Return the home page with download option allowed 
-        return render_template('index.html', download = True)
-    else:
-        
-        # Return the home page with download option not allowed 
+                    # if file invalid, return error message
+                    else:
+                        return render_template('index.html', download = False, message='Invalid file format!')
+                flash('saved files')
+                # iterate through the input images
+                for file in data:
+                    filename = secure_filename(file.filename)
+
+                    # read file as image using cv2
+                    image = cv2.imread(app.config['UPLOAD_FOLDER'] +'\\'+ filename)
+                    flash('took image in opencv')
+                    # convert into grayscale image
+                    imagebw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                    flash('converted to greyscale')
+                    # finds focus measure of the image
+                    ''' Focus measure is the degree of sharpness of an image i.e., so higher focus measure, more clear image.
+                    Focus measure is calculated by finding gradient of gaussian filter or a laplacian filter on an image. 
+                    Here, we use Laplacian filter. '''
+                    focus_measure = cv2.Laplacian(imagebw, cv2.CV_64F).var() 
+                    flash('calculated focus measure')
+                    # If focus measure < threshold, the image is blur, so save in blurred folder
+                    if focus_measure < threshold: 
+                        cv2.imwrite('./blurred/' + filename, image) 
+                        
+                    # If focus measure >= threshold, the image is clear, so save in clear folder
+                    else:
+                        cv2.imwrite('./clear/' + filename, image) 
+                    flash('saved images')
+            # Return the home page with download option allowed 
+            return render_template('index.html', download = True)
+        else:
+            
+            # Return the home page with download option not allowed 
+            return render_template('index.html', download = False)
+    except Exception as e:
+        flash(e)
         return render_template('index.html', download = False)
-
 # Download processed blur images
 @app.route('/download_blur', methods = ["POST", "GET"])
 def download_blur():
@@ -141,7 +144,7 @@ def download_blur():
 
         # Make a zip archive of blurred folder
         shutil.make_archive(dir_name, 'zip', dir_name)
-        
+        flash('zipped folder')
         # Download zip archive of blurred folder
         return send_file(dir_name + '.zip', as_attachment = True)
 
@@ -159,7 +162,7 @@ def download_clear():
 
         # Make a zip archive of clear folder
         shutil.make_archive(dir_name, 'zip', dir_name)
-
+        flash('zipped folder')
         # Download zip archive of clear folder
         return send_file(dir_name + '.zip', as_attachment = True)
 
